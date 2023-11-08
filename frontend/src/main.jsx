7f128ec4cb93fb9db5e5eef874ac104335ce7ae4
Valeriane SUDRE CHEVALIER <valeriane.chevalier@gmail.com>
2023-11-08 14:47:30 +0100

    creation de la page et de la route Basket

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,16 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import Basket from "./pages/Basket";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/basket",
+    element: <Basket />,
   },
 ]);
 
