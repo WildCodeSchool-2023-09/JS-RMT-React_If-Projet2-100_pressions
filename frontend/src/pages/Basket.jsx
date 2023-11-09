323b5381e699080ea6e9b81ec50a273c4c56f13b
Valeriane SUDRE CHEVALIER <valeriane.chevalier@gmail.com>
2023-11-09 16:37:33 +0100

    ajout de la page panier (Basket)

--- frontend/src/pages/Basket.jsx
+++ frontend/src/pages/Basket.jsx
@@ -4,4 +4,13 @@ function Basket() {
   return <div>Basket</div>;
 }
 
+// // fction saveBasket enregistrer panier dans le local storage (basket pour savoir quel panier enregistrer)
+// function saveBasket(basket) {
+//   localStorage.setItem("basket", basket);
+// }
+
+// function getBasket() {
+//   localStorage.getItem("basket");
+// }
+
 export default Basket;
